8f56120263b59d6d9e0709d32c22835aa9893d6a
CristianMartu <martuccicristian18@gmail.com>
2024-04-16 12:57:30 +0200

    index.js Cri
    12:57 16/04 pushato index.js to developeMain

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,81 +1,109 @@
-// window.onload = () => {
-//   handleFetch()
-// }
+// ricerca canzoni Cristian
+const inputSearch = document.getElementById('searchGeneral')
 
-// const handleFetch = async () => {
-//   try {
-//     const response = await fetch(url, options)
-//     const result = await response.json()
-//     read(result.data)
-//   } catch (error) {
-//     console.error(error)
-//   }
-// }
-
-// const read = (result) => {
-//   result.forEach((element) => {
-//     const title = element.title
-//     console.log(title)
-//     const type = element.type
-//     console.log(type)
-//     const artist = element.artist.name
-//     console.log(artist)
-//   })
-// }
-
-const btnCerca = document.getElementById('btnSearchGeneral')
-const inputCerca = document.getElementById('searchGeneral')
-
-function searchItems() {
-  const query = inputCerca.value.trim()
-  const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${encodeURIComponent(
-    query
-  )}`
-
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '50cb9d0834msh6916f2733fc1e75p1d758cjsnb17cfee22a1d',
-      'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
-    },
-  }
-
-  fetch(url, options)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('nessuna risposta')
-      }
-      return response.json()
-    })
-    .then((data) => {
-      console.log(data.data)
-      displayResults(data.data)
-    })
-    .catch((error) => {
-      console.error('Fetch error:', error.message)
-    })
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '50cb9d0834msh6916f2733fc1e75p1d758cjsnb17cfee22a1d',
+    'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
+  },
 }
 
-btnCerca.addEventListener('click', searchItems)
+window.onload = () => {
+  inputSearch.addEventListener('input', searchItems)
+}
 
-function displayResults(results) {
-  const containerTitoli = document.getElementById('containerTitoli')
-  if (results.length === 0) {
-    containerTitoli.innerHTML = `<p>Non ci sono risultati della ricerca</p>`
-    return
+const searchItems = (event) => {
+  const query = inputSearch.value.trim()
+  const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=
+                ${encodeURIComponent(query)}`
+  if (query !== '') {
+    handleFetch(url)
   }
-  let output = `<h2>Risultato più rilevante: <span class="fw-bold">${inputCerca.value.trim()}<span></h2>
+}
+
+const clearPage = () => {
+  document.getElementById('containerTitoli').value = ''
+  document.getElementById('containerArtist').value = ''
+  document.getElementById('containerAlbum').value = ''
+}
+
+const handleFetch = async (url) => {
+  try {
+    const response = await fetch(url, options)
+    if (response.ok) {
+      const result = await response.json()
+      // console.log(result.data)
+      clearPage()
+      if (result.data.length === 0) {
+        const containerTitoli = document.getElementById('containerTitoli')
+        containerAlbum.innerHTML = `<p>Non ci sono risultati della ricerca</p>`
+      } else {
+        displayTrackResults(result.data)
+        displayAlbumResults(result.data)
+        displayArtistResults(result.data)
+      }
+    } else {
+      throw new Error('Errore nella fetch')
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const displayTrackResults = (results) => {
+  const containerTitoli = document.getElementById('containerTitoli')
+  let output = `<h2>Risultato più rilevante: <span class="fw-bold">${inputSearch.value.trim()}<span></h2>
   <h3>Brani:</h3>`
-  output += `<div class="overflow-scroll overflow-x-hidden" style="height: 500px">`
+  output += `<div class="overflow-scroll overflow-x-hidden" style="height: 260px"><ul>`
   results.forEach((element) => {
-    output += '<ul>'
-    output += `<li><div class="d-flex"><img style="border-radius: 4px;" src="${element.album.cover}" alt="immagine inerente al brano" height="50"><div class="ms-2"><p class="fw-bold m-0 mb-2" style="font-size: 15px;">${element.title}</p><p class="m-0" style="font-size: 12px;">${element.artist.name}</p></div></div></li>
+    output += `<li class="rounded cardContainer">
+                <div class="d-flex align-items-center py-2 ps-2">
+                  <img style="border-radius: 4px;" src="${element.album.cover}" alt="immagine inerente al brano" height="50">
+                  <div class="ms-2">
+                    <p class="fw-bold m-0 mb-2" style="font-size: 15px;">${element.title}</p>
+                    <p class="m-0" style="font-size: 12px;">${element.artist.name}</p>
+                  </div>
+                </div>
+              </li>
     `
-    output += '</ul>'
   })
-  output += '</div>'
+  output += '</ul></div>'
   containerTitoli.innerHTML = output
 }
+
+const displayArtistResults = (results) => {
+  const containerArtist = document.getElementById('containerArtist')
+  let output = `<h3 class="mt-4">Artisti:</h3>`
+  output += `<div class="overflow-scroll overflow-x-hidden d-flex flex-wrap justify-content-evenly" style="height: 165px">`
+  results.forEach((element) => {
+    output += `<div class="my-2 py-1 px-2 rounded cardContainer">
+                <img style="border-radius: 50%;" src="${element.artist.picture}" height="100">
+                <p class="fw-bold m-0 mb-2" style="font-size: 15px;">${element.artist.name}</p>
+                <p class="m-0" style="font-size: 12px;">Artista</p>
+              </div>`
+  })
+  output += '</div>'
+  containerArtist.innerHTML = output
+}
+
+const displayAlbumResults = (results) => {
+  const containerAlbum = document.getElementById('containerAlbum')
+  let output = `<h3 class="mt-4">Album:</h3>`
+  output += `<div class="overflow-scroll overflow-x-hidden d-flex flex-wrap justify-content-evenly" style="height: 120px">`
+  results.forEach((element) => {
+    output += `<a href="assets/html/albumpage.html?albumId=${element.album.id}">
+                <div class="py-2 px-2 rounded cardContainer">
+                  <img style="border-radius: 4px;" src="${element.album.cover}" height="50px">
+                  <p class="fw-bold m-0 mb-2" style="font-size: 15px;">${element.artist.name}</p>
+                  <p class="m-0" style="font-size: 12px;">Album</p>
+                </div>
+              </a>`
+  })
+  output += '</div>'
+  containerAlbum.innerHTML = output
+}
+// fine ricerca Cristian
 
 //FUNZIONE PER AGGIUNGERE UNA PLAYLIST CON EFFETTO DI FADE-IN
 function addPlaylistWithFadeIn(playlistHTML) {
