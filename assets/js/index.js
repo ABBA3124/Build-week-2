cd5bd624827dd18042414fe285164765f3e78a69
Yahia Lakrikba <161433620+YahiaLakrikba2004@users.noreply.github.com>
2024-04-16 11:20:24 +0200

    playlist
    Sistemata la parte delle playlist e le icone

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,67 +23,60 @@
 //   })
 // }
 
-const btnCerca = document.getElementById("btnSearchGeneral");
-const inputCerca = document.getElementById("searchGeneral");
+const btnCerca = document.getElementById('btnSearchGeneral')
+const inputCerca = document.getElementById('searchGeneral')
 
 function searchItems() {
-  const query = inputCerca.value.trim();
-  const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${encodeURIComponent(query)}`;
+  const query = inputCerca.value.trim()
+  const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${encodeURIComponent(
+    query
+  )}`
 
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "50cb9d0834msh6916f2733fc1e75p1d758cjsnb17cfee22a1d",
-      "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+      'X-RapidAPI-Key': '50cb9d0834msh6916f2733fc1e75p1d758cjsnb17cfee22a1d',
+      'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com',
     },
-  };
+  }
 
   fetch(url, options)
     .then(response => {
       if (!response.ok) {
-        throw new Error('nessuna risposta');
+        throw new Error('nessuna risposta')
       }
-      return response.json();
+      return response.json()
     })
     .then(data => {
-      console.log(data.data);
-      displayResults(data.data);
+      console.log(data.data)
+      displayResults(data.data)
     })
     .catch(error => {
-      console.error('Fetch error:', error.message);
-    });
+      console.error('Fetch error:', error.message)
+    })
 }
 
-btnCerca.addEventListener("click", searchItems);
+btnCerca.addEventListener('click', searchItems)
 
 function displayResults(results) {
-  const containerTitoli = document.getElementById("containerTitoli");
+  const containerTitoli = document.getElementById('containerTitoli')
   if (results.length === 0) {
-    containerTitoli.innerHTML = `<p>Non ci sono risultati della ricerca</p>`;
-    return;
+    containerTitoli.innerHTML = `<p>Non ci sono risultati della ricerca</p>`
+    return
   }
   let output = `<h2>Risultato più rilevante: <span class="fw-bold">${inputCerca.value.trim()}<span></h2>
-  <h3>Brani:</h3>`;
-  output += `<div class="overflow-scroll overflow-x-hidden" style="height: 500px">`;
+  <h3>Brani:</h3>`
+  output += `<div class="overflow-scroll overflow-x-hidden" style="height: 500px">`
   results.forEach(element => {
-    output += "<ul>";
+    output += '<ul>'
     output += `<li><div class="d-flex"><img style="border-radius: 4px;" src="${element.album.cover}" alt="immagine inerente al brano" height="50"><div class="ms-2"><p class="fw-bold m-0 mb-2" style="font-size: 15px;">${element.title}</p><p class="m-0" style="font-size: 12px;">${element.artist.name}</p></div></div></li>
-    `;
-    output += "</ul>";
-  });
-  output += "</div>";
-  containerTitoli.innerHTML = output;
+    `
+    output += '</ul>'
+  })
+  output += '</div>'
+  containerTitoli.innerHTML = output
 }
 
-
-
-
-
-
-
-
-
-   
-    
-
-{/* <li><div><img src="${element.album.cover_small}" alt="immagine inerente al brano"><div><h3>${element.title}</h3><p>${element.artist.name}</p></div></div></li> */}
+{
+  /* <li><div><img src="${element.album.cover_small}" alt="immagine inerente al brano"><div><h3>${element.title}</h3><p>${element.artist.name}</p></div></div></li> */
+}
