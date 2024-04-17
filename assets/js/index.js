09bd7a22a876841f69f262e81d14fc0fb2f58f5d
CristianMartu <martuccicristian18@gmail.com>
2024-04-17 11:16:56 +0200

    album merge into index

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,17 @@
+const params = new URLSearchParams(window.location.search)
+const id = params.get('albumId')
+const URL_API = 'https://deezerdevs-deezer.p.rapidapi.com/album/' + id
+const KEY = 'bc10829072mshb658932a6a8dddep1366a1jsn27a0e20c2896'
+const HOST = 'deezerdevs-deezer.p.rapidapi.com'
+
 // start home page automatica random
 document.addEventListener('DOMContentLoaded', () => {
-  searchRandomTracks()
+  if (id) {
+    console.log(URL_API)
+    createAlbum()
+  } else {
+    searchRandomTracks()
+  }
 })
 
 homeButton.addEventListener('click', function () {
@@ -107,16 +118,22 @@ function displayAlbumResults2(results, query) {
   let output = `<h3 class="mt-4">Album</h3>`
   output += `<div class="row" >`
   results.forEach((element) => {
-    output += `<a class="col-3" href="assets/html/albumpage.html?albumId=${element.album.id}">
-                <div class=" rounded cardContainer text-left">
-                  <img style="border-radius: 6px;" src="${element.album.cover}" height="150px">
-                  <p class="mb-1 fw-bold " style="font-size: 12px;">${element.album.title}</p>
-                  <p class="mt-0" style="font-size: 10px;">2018 • ${element.artist.name}</p>
-                </div>
-              </a>`
+    output += `<a class="col-3 albumCard" href="../../../index.html?albumId=${element.album.id}">
+    <div class=" rounded cardContainer text-left">
+      <img style="border-radius: 6px;" src="${element.album.cover}" height="150px">
+      <p class="mb-1 fw-bold " style="font-size: 12px;">${element.album.title}</p>
+      <p class="mt-0" style="font-size: 10px;">2018 • ${element.artist.name}</p>
+    </div>
+  </a>`
   })
   output += `</div>`
   containerAlbum.innerHTML = output
+
+  // const cards = document.querySelectorAll('.albumCard')
+  // cards.forEach((album) => {
+  //   album.addEventListener('click', createAlbum)
+
+  // })
 }
 // fine home page automatica random
 
@@ -241,7 +258,7 @@ function displayAlbumResults(results, query) {
   let output = `<h3 class="mt-4">Album</h3>`
   output += `<div class="row" >`
   results.forEach((element) => {
-    output += `<a class="col-3" href="assets/html/albumpage.html?albumId=${element.album.id}">
+    output += `<a class="col-3" href="../../../index.html?albumId=${element.album.id}">
                 <div class=" rounded cardContainer text-left">
                   <img style="border-radius: 6px;" src="${element.album.cover}" height="150px">
                   <p class="mb-1 fw-bold " style="font-size: 12px;">${element.album.title}</p>
@@ -362,3 +379,163 @@ document.querySelectorAll('.playlist').forEach((item) => {
 // end yahia
 
 // lavoro Cri
+
+const createAlbum = () => {
+  // document.getElementById('containerTitoli').innerHTML = ''
+  // document.getElementById('containerArtist').innerHTML = ''
+  // document.getElementById('containerAlbum').innerHTML = ''
+
+  const pos = document.getElementById('containerLaunchHomePage')
+  pos.innerHTML = `
+  <div id="hero" class="d-flex mt-3 mt-md-5 ms-md-4">
+  <div class="img ms-3 me-3 me-md-4" id="hero-img">
+    <img src="" alt="" id="img-cover" />
+  </div>
+  <div class="info d-flex flex-column justify-content-between">
+    <p class="display-6">ALBUM</p>
+    <h1 id="album-title"></h1>
+    <div id="info-cantante ">
+      <img src="" alt="" id="img-artist" />
+      <span id="info-album"></span>
+    </div>
+  </div>
+</div>
+<hr />
+<button class="mb-3 ms-3 btn me-3" id="btn-play">
+  <svg
+    data-encore-id="icon"
+    role="img"
+    fill="white"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    class="Svg-sc-ytk21e-0 bneLcE"
+  >
+    <path
+      d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"
+    ></path>
+  </svg>
+</button>
+
+<button class="mb-3 btn btn-secondary" id="btn-save">
+  <svg
+    data-encore-id="icon"
+    role="img"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    class="Svg-sc-ytk21e-0 cqasRA"
+    fill="white"
+  >
+    <path
+      d="M5.21 1.57a6.757 6.757 0 0 1 6.708 1.545.124.124 0 0 0 .165 0 6.741 6.741 0 0 1 5.715-1.78l.004.001a6.802 6.802 0 0 1 5.571 5.376v.003a6.689 6.689 0 0 1-1.49 5.655l-7.954 9.48a2.518 2.518 0 0 1-3.857 0L2.12 12.37A6.683 6.683 0 0 1 .627 6.714 6.757 6.757 0 0 1 5.21 1.57zm3.12 1.803a4.757 4.757 0 0 0-5.74 3.725l-.001.002a4.684 4.684 0 0 0 1.049 3.969l.009.01 7.958 9.485a.518.518 0 0 0 .79 0l7.968-9.495a4.688 4.688 0 0 0 1.049-3.965 4.803 4.803 0 0 0-3.931-3.794 4.74 4.74 0 0 0-4.023 1.256l-.008.008a2.123 2.123 0 0 1-2.9 0l-.007-.007a4.757 4.757 0 0 0-2.214-1.194z"
+    ></path>
+  </svg>
+</button>
+<div class="row border-bottom border-secondary ms-2 mb-md-4">
+  <div class="col-5 col-md-6"># Titolo</div>
+  <div class="col-4 col-md-4">Riproduzioni</div>
+  <div class="col-3 col-md-2">tempo</div>
+</div>
+<div id="songs"></div>                
+  `
+  fetchAlbum()
+}
+
+const generateSong = (titolo, artista, riproduzioni, tempo) => {
+  const divSongs = document.getElementById('songs')
+  // div generale della card
+  const divCard = document.createElement('div')
+  divCard.classList.add('row', 'card-song')
+
+  // title della card
+  const divTitle = document.createElement('div')
+  divTitle.classList.add('col-5', 'col-md-6', 'my-md-1', 'fw-bold')
+  divTitle.innerText = titolo
+  const pArtista = document.createElement('p')
+  pArtista.classList.add('fw-normal')
+  pArtista.innerText = artista
+  divTitle.appendChild(pArtista)
+  divCard.appendChild(divTitle)
+  // riproduzioni
+  const divRiproduzioni = document.createElement('div')
+  divRiproduzioni.classList.add('col-4', 'col-md-4')
+  divRiproduzioni.innerText = riproduzioni
+  divCard.appendChild(divRiproduzioni)
+
+  // durata canzone
+  tempo = Math.ceil(tempo / 60)
+  const divDurataCanzone = document.createElement('div')
+  divDurataCanzone.classList.add('col-3', 'col-md-2')
+  divDurataCanzone.innerText = `${tempo} min`
+  divCard.appendChild(divDurataCanzone)
+
+  divSongs.appendChild(divCard)
+}
+
+const fetchAlbum = () => {
+  fetch(URL_API, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': KEY,
+      'X-RapidAPI-Host': HOST,
+    },
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .then((data_response) => {
+      console.log(data_response)
+
+      //   Immagine cover
+      const imageLinkCover = data_response.cover_medium
+      const imgCover = document.getElementById('img-cover')
+      imgCover.setAttribute('src', imageLinkCover)
+
+      // titolo Album
+      const h1 = document.getElementById('album-title')
+      h1.innerText = data_response.title
+
+      //  INFO ALBUM
+      // IMMAGINE ARTISTA
+      const IMG_LINK_ARTIST = data_response.artist.picture_small
+      const imgArtist = document.getElementById('img-artist')
+      imgArtist.setAttribute('src', IMG_LINK_ARTIST)
+      // RESTO DELLE INFO DELL'ARTISTA
+      const durationTracks = (data_response.duration / 60).toString().split('.')
+      let min = durationTracks[0]
+      min = parseInt(min)
+
+      let sec = durationTracks[1]
+      sec = sec.substring(0, 2)
+      sec = parseInt(sec)
+
+      const tracks = data_response.nb_tracks
+      const titoloCantante = data_response.artist.name
+      const releaseDate = new Date(data_response.release_date).getFullYear()
+      const spanInfoAlbum = document.getElementById('info-album')
+      const spanTracks = document.createElement('span')
+      const spanReleaseDate = document.createElement('span')
+      const spanTitoloCantante = document.createElement('span')
+      const spanTime = document.createElement('span')
+
+      spanTracks.innerText = `${tracks} Brani ◦ `
+      spanTitoloCantante.innerText = `${titoloCantante} ◦ `
+      spanReleaseDate.innerText = `${releaseDate} ◦ `
+      spanTime.innerText = `${min}min ${sec}sec`
+      spanInfoAlbum.appendChild(spanTitoloCantante)
+      spanInfoAlbum.appendChild(spanTracks)
+      spanInfoAlbum.appendChild(spanReleaseDate)
+      spanInfoAlbum.appendChild(spanTime)
+
+      //   FINE PARTE INFO ALBUM
+
+      // INIZIO PARTE TRACKS
+      // genera tutte le card in base a quante canzoni cu sono nell'album
+      data_response.tracks.data.forEach((song) => {
+        generateSong(song.title, song.artist.name, song.rank, song.duration)
+      })
+      // FINE PARTE TRACKS
+
+      // click su una traccia
+      // Inserire la funzione qui
+    })
+}
