e4d5df8d77125bb5fc564f087c1eee1e1fa3d921
Yahia Lakrikba <161433620+YahiaLakrikba2004@users.noreply.github.com>
2024-04-17 16:41:50 +0200

    modifiche giuste
    sistemato alcuni errori

--- assets/js/artistpage.js
+++ assets/js/artistpage.js
@@ -13,7 +13,7 @@ let lastUserEventTimes = Array.from(
 window.addEventListener('click', () => {
   lastUserEventTimes = lastUserEventTimes.map(() => new Date().getTime())
 })
-
+//AGGIONRA SCRITTA BADGE
 function updateBadgeTime() {
   const badges = document.querySelectorAll('.last-activity')
 
@@ -23,7 +23,6 @@ function updateBadgeTime() {
     const timeDifference = Math.floor((currentTime - lastUserEventTime) / 1000)
 
     if (index < 2) {
-      // Se l'utente è il primo o il secondo, calcola il tempo trascorso dall'ultimo evento e visualizzalo come minuti o ore fa
       const minutesAgo = Math.floor(timeDifference / 60)
       if (minutesAgo === 0) {
         badge.innerText = 'Online'
@@ -31,12 +30,8 @@ function updateBadgeTime() {
         badge.classList.add('bg-success')
       } else if (minutesAgo === 1) {
         badge.innerText = '1 min fa'
-        badge.classList.remove('bg-dark')
-        badge.classList.add('bg-success')
       } else if (minutesAgo < 60) {
         badge.innerText = minutesAgo + ' min fa'
-        badge.classList.remove('bg-dark')
-        badge.classList.add('bg-success')
       } else {
         const hoursAgo = Math.floor(minutesAgo / 60)
         if (hoursAgo === 1) {
@@ -44,22 +39,27 @@ function updateBadgeTime() {
         } else {
           badge.innerText = hoursAgo + ' ore fa'
         }
+      }
 
-        // Se sono passati esattamente 60 secondi, cambia il colore del badge
-        if (timeDifference === 60) {
-          badge.classList.remove('bg-success')
-          badge.classList.add('bg-dark')
-        }
+      // DOPO 60 SEC, CAMBIA COLORE BACKGROUND
+      if (timeDifference === 60) {
+        badge.classList.remove('bg-success')
+        badge.classList.add('bg-dark')
       }
     } else {
-      // Se l'utente è il terzo, quarto o quinto e non è passato più di un'ora dall'ultimo evento, consideralo online
       if (timeDifference < 3600) {
         badge.innerText = 'Online'
         badge.classList.remove('bg-dark')
         badge.classList.add('bg-success')
       } else {
         // Altrimenti, visualizza l'ultimo evento come "Offline"
-        badge.innerText = 'Offline'
+        if (index === lastUserEventTimes.length - 2) {
+          badge.innerText = '14 ore fa' // Ultimo evento del penultimo utente 14 ore fa
+        } else if (index === lastUserEventTimes.length - 1) {
+          badge.innerText = '3 ore fa' // Ultimo evento dell'ultimo utente 3 ore fa
+        } else {
+          badge.innerText = 'Offline'
+        }
         badge.classList.remove('bg-success')
         badge.classList.add('bg-dark')
       }
